d9b4ab94d81746dbb3cdb5075948fc4a38855559
graduate2024test <graduate2024test@gmail.com>
2024-04-24 14:03:48 +0400

    routing v0

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,24 @@
 const express = require("express");
+
+/* ********************************************* */
+/* *********************************** CONSTANTS */
 const app = express();
+const port = 3000;
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+/* ********************************************* */
+/* *********************************** FUNCTIONS */
+
+/* ********************************************* */
+/* *********************************** ROUTING ***/
+app.get("/", (req, res) => res.send("BACKSLASH"));
+
+app.get("/map", (req, res) => res.send("MAP"));
+
+app.get("/room", (req, res) => res.send("ROOM"));
+
+app.get("/timetable", (req, res) => res.send("TIMETABLE"));
+
+app.listen(port, () => console.log("LISTEN 3000."));
 
 module.exports = app;
